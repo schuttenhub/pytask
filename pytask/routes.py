d56ecd1ab522e0fe517b9e664ba82ad70f6d956b
schuttenhub <schuttenhub@hs-albsig.de>
2024-04-22 21:20:30 +0200

    /login route prints entfernt

--- pytask/routes.py
+++ pytask/routes.py
@@ -13,6 +13,7 @@ def index():
 
 @app.route('/tasks')
 def tasks():
+    query = f"select * from t"
     return render_template('tasks.html')
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -21,34 +22,19 @@ def login_action():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        print("Here the Data!!!")
-        print(username)
-        print(password)
 
-        if (username is None or
-                isinstance(username, str) is False or
-                len(username) < 3):
-            print("not valid")
-            #flash(f"Username is not valid", category='warning')
+        if (username is None or isinstance(username, str) is False or len(username) < 3):
             return render_template('login.html')
 
-        if (password is None or
-                isinstance(password, str) is False or
-                len(password) < 3):
-            print("something with password")
-            #flash(f"Password is not valid", category='warning')
+        if (password is None or isinstance(password, str) is False or len(password) < 3):
             return render_template('login.html')
 
         query_stmt = f"select username from user where username = '{username}' and password = '{password}'"
-        print(query_stmt)
         result = db.session.execute(text(query_stmt))
 
         user = result.fetchall()
-        print(user)
         if not user:
-            #flash(f"Try again", category='warning')
             return render_template('login.html')
-        #flash(f"'{user}', you are logged in ", category='success')
         return redirect(url_for('tasks'))
     else:
         print("no post...")
@@ -57,30 +43,17 @@ def login_action():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        print('Post')
-
         email = request.form.get('email')
         username = request.form.get('username')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        print(email)
-        print(username)
-        print(password1)
-        print(password2)
-
-        if(username is None or
-                isinstance(username, str) is False or
-                len(username) < 3):
+        if(username is None or isinstance(username, str) is False or len(username) < 3):
             print("username not valid")
-            #flash("Username not valid", category='danger')
             return render_template('register.html')
 
-        if(email is None or
-                isinstance(email, str) is False or
-                len(email) < 3):
+        if(email is None or isinstance(email, str) is False or len(email) < 3):
             print("email not valid")
-            #flash("Email not valid", category='danger')
             return render_template('register.html')
 
         if(password1 is None or
@@ -88,27 +61,20 @@ def register():
                 len(password1) < 3 or
                 password1 != password2):
             print("password1 not valid")
-            #flash("Password1 not valid", category='danger')
             return render_template('register.html')
 
         query_stmt = f"select * from user where username = '{username}'"
-        print(query_stmt)
         result = db.session.execute(text(query_stmt))
         item = result.fetchone()
-        print(item)
 
         if item is not None:
-            #flash("Username exists, try again")
             print("Username exists")
             return render_template('register.html')
 
         query_insert = f"insert into user (username, password, email) values ('{username}', '{password1}', '{email}')"
-        print(query_insert)
         db.session.execute(text(query_insert))
         db.session.commit()
-        #flash("You are registered", category='success')
         return redirect(url_for('tasks'))
-
     return render_template('register.html')
 
 @app.route('/logout')
