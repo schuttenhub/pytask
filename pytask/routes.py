e7650141d41fbca3084b3cb9a5961d8f87383ecc
schuttenhub <schuttenhub@hs-albsig.de>
2024-04-23 10:26:49 +0200

    Cookies erflogreich eingebaut für login. Logout und sidebar eingebaut in Login Status. Nächste Todos: - [ ] Projekte in die Sidebar packen je nach user - [ ] Button machen um neue projekte anzulegen

--- pytask/routes.py
+++ pytask/routes.py
@@ -1,24 +1,42 @@
 from pytask import app, db
-from flask import render_template, request, flash, url_for, redirect, jsonify
+from flask import render_template, request, flash, url_for, redirect, jsonify, make_response
 from sqlalchemy import text
+
+logged_in = False
+
+def return_site(site_name):
+    logged_in = True if request.cookies.get('user_name') else False
+    resp = render_template(f'{site_name}', logged_in = logged_in)
+    response = make_response(resp)
+    print("Login Status =", logged_in)
+    return response
 
 @app.route('/')
 def index():
-    #todos = Todo.query.join(Project).filter(Project.id == 1, Todo.is_completed == False)()
-    #todos = Todo.query.join(Project).filter(Project.id == 1, Todo.is_completed == False).all()
-    #unfinished_todos = Todo.query.filter_by(is_completed=False).all()
-    #return render_template('app.html', todos=todos, unfinished_todos=unfinished_todos)
-    #cookie = request.cookies.get('name') --> Dann auch bei return die variable übergeben
-    return render_template('welcome.html')
+    return return_site('welcome.html')
 
 @app.route('/tasks')
 def tasks():
-    query = f"select * from t"
-    return render_template('tasks.html')
+    #get user_id 
+    user_id = request.cookies.get('user_id')
+    #get projects
+    query_projects = f"select * from project where id = '{user_id}'"
+    result_projects = db.session.execute(text(query_projects))
+    projects = result_projects.fetchall()
+
+    # Jetzt Projekte in die sidebar packen
+
+    # Dann Button machen, der neue Projekte in der Sidebar erstellen kann
+
+
+    logged_in = True if request.cookies.get('user_name') else False
+    resp = render_template(f'{site_name}', logged_in = logged_in)
+    response = make_response(resp)
+    print("Login Status =", logged_in)
+    return response
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_action():
-    print("login was called")
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
@@ -28,15 +46,20 @@ def login_action():
 
         if (password is None or isinstance(password, str) is False or len(password) < 3):
             return render_template('login.html')
-
-        query_stmt = f"select username from user where username = '{username}' and password = '{password}'"
+        
+        query_stmt = f"select username, id from user where username = '{username}' and password = '{password}'"
         result = db.session.execute(text(query_stmt))
         user = result.fetchall()
+
         if not user:
-            return render_template('login.html')
-        return redirect(url_for('tasks'))
-    else:
-        print("no post...")
+            return render_template('login.html', )
+        user_id = user[0][1]
+        response = make_response(redirect(url_for('tasks')))
+        response.set_cookie('user_name', f'{username}')
+        response.set_cookie('user_id', f'{user_id}')
+        return response
+    
+    #else Fall
     return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -79,5 +102,6 @@ def register():
 @app.route('/logout')
 def logout():
     resp = redirect('/')
-    resp.set_cookie('name', '', 0)
+    resp.set_cookie('user_name', '', 0)
+    resp.set_cookie('user_id', '', 0)
     return resp
