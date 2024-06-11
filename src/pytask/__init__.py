27cb8c9e71dd450509ace1ae144f6729a7bbf5ad
schuttenhub <schuttenhub@hs-albsig.de>
2024-06-11 11:06:54 +0200

    more docker things

--- src/pytask/__init__.py
+++ src/pytask/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 import pymysql
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@127.0.0.1/pytaskdb'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@127.0.0.1/pytaskdb' --> OLD
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'mysql+pymysql://root:root@localhost/pytaskdb') # --> NEW
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SESSION_COOKIE_HTTPONLY'] = False
 app.secret_key = 'abcdef1234560000'
