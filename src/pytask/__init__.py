1ba28bd3ac66b2ce60655a5eba6dab37fa4f19dd
schuttenhub <schuttenhub@hs-albsig.de>
2024-06-27 17:07:10 +0200

    test

--- src/pytask/__init__.py
+++ src/pytask/__init__.py
@@ -7,7 +7,7 @@ import pymysql
 app = Flask(__name__)
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@127.0.0.1/pytaskdb' --> OLD
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'mysql+pymysql://root:root@localhost/pytaskdb') # --> NEW
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'mysql+pymysql://root:root@db/pytaskdb') # --> NEW
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SESSION_COOKIE_HTTPONLY'] = False
 app.secret_key = '3oXa26gmb4FrtrHQwsGBqBUs9Ht'
